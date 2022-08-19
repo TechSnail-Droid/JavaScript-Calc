6fdd5ed286234215d4823ff7a55fdbff2f1f1c82
TechSnail-Droid <107436055+TechSnail-Droid@users.noreply.github.com>
2022-08-19 16:21:46 -0400

    Add files via upload
    Updated functionality, css, and design of the project

--- calculator.js
+++ calculator.js
@@ -12,7 +12,8 @@ numbers.forEach(number => {
     number.addEventListener("click", e =>{
         if (operator === ""){
             int1 += e.target.innerText; 
-              
+           
+          
         } 
         else {
             int2 += e.target.innerText;
@@ -35,7 +36,17 @@ operators.forEach(op =>{
             
         } else {
             display.append(int2);
+            if (int2 == 0 && operator == "/"){
+                alert('Don\'t try that');
+                
+            }
             switch (operator){
+                case "!":
+                    display.append(" = " + fac(int1))
+                    break;
+                case "SQRT":
+                    display.append(" = " + sqrt(int1))
+                    break;
                 case "+":
                    display.append(" = " + addition(int1, int2))
                    break;
@@ -57,46 +68,74 @@ operators.forEach(op =>{
 });
 
 function addition(int1, int2){
-    if(int1 === int1 && int1 % 1 !== 0){
+    if((int1 === int1 && int1 % 1 !== 0)|| (int2 === int2 && int2 % 1 !== 0)){
         value = (parseFloat(int1) + parseFloat(int2))
+        roundedValue = value.toFixed(2)
+        realValue = roundedValue
     }
     else if (int1 === int1 && int1 % 1 ===0){
         value = (parseInt(int1) + parseInt(int2))
+        realValue = value
     }
    
-    roundedValue = value.toFixed(2)
-    return roundedValue
+    
+    return realValue
 }
 function subtract(int1,int2){
-    if(int1 === int1 && int1 % 1 !== 0){
+    if(int1 === int1 && int1 % 1 !== 0 || (int2 === int2 && int2 % 1 !== 0)){
         value = (parseFloat(int1) - parseFloat(int2))
+        roundedValue = value.toFixed(2)
+        realValue = roundedValue
     }
     else if (int1 === int1 && int1 % 1 ===0){
         value = (parseInt(int1) - parseInt(int2))
+        realValue = value
     }
-    roundedValue = value.toFixed(2)
-    return roundedValue
+    return realValue
 }
-function multiply(int1, int2){
-    if(int1 === int1 && int1 % 1 !== 0){
+function multiply(int1, int2 ){
+    if(int1 === int1 && int1 % 1 !== 0 || (int2 === int2 && int2 % 1 !== 0)){
         value = (parseFloat(int1) * parseFloat(int2))
+        roundedValue = value.toFixed(2)
+        realValue = roundedValue
     }
     else if (int1 === int1 && int1 % 1 ===0){
         value = (parseInt(int1) * parseInt(int2))
+        realValue = value
     }
-    roundedValue = value.toFixed(2)
-    return roundedValue
+    return realValue
     
 }
 function divide(int1, int2){
-    if(int1 === int1 && int1 % 1 !== 0){
+    if(int1 === int1 && int1 % 1 !== 0 || (int2 === int2 && int2 % 1 !== 0)){
         value = (parseFloat(int1) / parseFloat(int2))
+        roundedValue = value.toFixed(2)
+        realValue = roundedValue
     }
     else if (int1 === int1 && int1 % 1 ===0){
         value = (parseInt(int1) / parseInt(int2))
+        realValue = value
     }
-    roundedValue = value.toFixed(2)
-    return roundedValue
+    return realValue
+}
+
+function sqrt (int1){
+    value = Math.sqrt(int1)
+    if (value === value && value % 1 !== 0){
+        roundedValue = value.toFixed(2)
+        realValue = roundedValue
+    }
+    realValue = value
+    return realValue
+}
+function fac (int1){
+    if (int1 === 0 || int1 === 1){
+        return 1
+    }
+    else {
+        return int1 * fac(int1 - 1)
+        
+    }
 }
 
 /* function printDisplay(displayValue){
@@ -110,8 +149,8 @@ display.append(element);
 
 }
 */
-function clearDisplay(elementID){
+ function clearDisplay(elementID){
    document.getElementById(elementID).innerText="";
    document.getElementById(elementID).innerHTML="";
   
-}
+ }
